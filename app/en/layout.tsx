47346c7ddcabb361f23e3da4609a0cc170b5cc2a
yandong <tony.yandong@hotmail.com>
2026-08-23 20:55:55 +0800

    fix: load the English language control styles

--- app/en/layout.tsx
+++ app/en/layout.tsx
@@ -1,3 +1,5 @@
+import "./language-switch.css";
+
 export default function EnglishLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return <div lang="en" dir="ltr">{children}</div>;
 }
