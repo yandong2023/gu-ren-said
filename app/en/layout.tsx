5e2e482a2d86df5f7b602e28e31815faa76850a2
yandong <tony.yandong@hotmail.com>
2026-08-23 21:17:31 +0800

    refactor: use the shared language dropdown on English pages

--- app/en/layout.tsx
+++ app/en/layout.tsx
@@ -1,5 +1,3 @@
-import "./language-switch.css";
-
 export default function EnglishLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return <div lang="en" dir="ltr">{children}</div>;
 }
